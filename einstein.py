97b921043233528509fba80831b52552b5d9a6ec
kaerber <kaerber@gmail.com>
2023-03-21 21:08:01 +0100

    initial commit

---
+++ einstein.py
@@ -0,0 +1,249 @@
+
+
+class Attr:
+    def __init__(self, name, order, values):
+        self.name = name
+        self.order = order
+        self.ordered_values = [AttrValue(self, idx, v) for (idx, v) in zip(range(len(values)), values)]
+        self.values = {v.value: v for v in self.ordered_values }
+
+    def value_at(self, index):
+        if index >= 0 and index < len(self.ordered_values):
+            return self.ordered_values[index]
+        return None
+
+    def __getattr__(self, name):
+        if name in self.values:
+            return self.values[name]
+        raise AttributeError(name)
+
+    def __getitem__(self, name):
+        return self.values[name]
+
+    def __hash__(self):
+        return hash((self.name, self.order))
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return str(self)
+
+
+class AttrValue:
+    def __init__(self, attr, index, value):
+        self.attr = attr
+        self.index = index
+        self.value = value
+
+    def offset_value(self, offset):
+        return self.attr.value_at(self.index + offset)
+
+    def __hash__(self):
+        return hash((self.attr, self.value))
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        return self.attr == other.attr and self.value == other.value
+
+    def __str__(self):
+        return str(self.attr) + "[" + self.index + "]" +  ":" + self.value
+
+    def __repr__(self):
+        return str(self)
+
+
+class Relation:
+    def __init__(self, atval1, atval2):
+        assert(atval1.attr != atval2.attr)
+        if atval1.attr.order < atval2.attr.order:
+            self.atval1 = atval1
+            self.atval2 = atval2
+        else:
+            self.atval1 = atval2
+            self.atval2 = atval1
+
+    def with_attr(self, attr):
+        return self.atval1.attr == attr or self.atval2.attr == attr
+
+    def with_atval(self, atval):
+        return self.atval1 == atval or self.atval2 == atval
+
+    def atval(self, attr):
+        if self.atval1.attr == attr:
+            return self.atval1
+        if self.atval2.attr == attr:
+            return self.atval2
+        return None
+
+    def __hash__(self) -> int:
+        return hash((self.atval1, self.atval2))
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        return self.atval1 == other.atval1 and self.atval2 == other.atval2
+
+
+class Solver:
+    def __init__(self):
+        self.map = {}
+        self.list = []
+
+    def add(self, rule):
+        self.list.append(rule)
+        if rule.relation != None:
+            self.map[rule.relation] = rule
+        return self
+    
+    def __contains__(self, relation):
+        return relation in self.map
+
+    def get(self, relation):
+        if relation not in self.map:
+            return None
+        return self.map[relation]
+
+    def is_same(self, relation):
+        return relation in self.map and isinstance(self.map[relation], Same)
+
+    def is_different(self, relation):
+        return relation in self.map and isinstance(self.map[relation], Different)
+
+
+class Exclusive:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def evaluate(self, relation):
+        a1 = relation.atval1
+        a2 = relation.atval2
+        if self.check_all_filled(a1, a2) or self.check_all_filled(a2, a1):
+            return Same(relation)
+
+    def check_all_filled(self, fixed: AttrValue, sliding: AttrValue):
+        for slider in sliding.attr.values.values():
+            if slider == sliding:
+                continue
+            if not self.matrix.is_different(Relation(fixed, slider)):
+                return False
+        return True
+
+class Offset:
+    # дом не такой = дом не существует, или не такой
+
+    # Зелёный дом стоит сразу справа от белого дома.
+    # - если дом X-1 - не белый, то дом X - не зеленый| (X, зеленый): if ~(X-1, белый) -> Different
+    # - если дом X+1 - не зеленый, то дом X - не белый| (X, белый): if ~(X+1, зеленый) -> Different
+
+    # matrix - матрица связей
+    # atval1 - значение атрибута первой сущности в паре (цвет:зеленый для загадки Эйнштейна)
+    # atval2 - значение атрибута второй сущности в паре (цвет:белый для загадки Эйнштейна)
+    # offset_attr - атрибут, по которому считается смещение (порядок_домов для загадки Эйнштейна)
+    # offset - смещение atval1 от atval2 по offset_attr (+1 для загадки Эйнштейна), может быть отрицательным, не может быть 0
+    def __init__(self, matrix, atval1, atval2, offset_attr, offset):
+        assert(offset != 0)
+
+        self.matrix = matrix
+        self.atval1 = atval1
+        self.atval2 = atval2
+        self.offset_attr = offset_attr
+        self.offset = offset
+
+    def evaluate(self, relation):
+        if not relation.with_attr(self.offset_attr):
+            return None
+
+        cur_offset_val = relation.atval(self.offset_attr)
+        
+        if relation.with_atval(self.atval1):
+            offset_val = cur_offset_val.offset_value(-self.offset)
+            if (offset_val == None
+                or self.matrix.is_different(Relation(offset_val, self.atval2))):
+                return Different(relation)
+
+        if relation.with_atval(self.atval2):
+            offset_val = cur_offset_val.offset_value(+self.offset)
+            if (offset_val == None
+                or self.matrix.is_different(Relation(offset_val, self.atval1))):
+                return Different(relation)
+
+class Distance:
+    # дом не такой = дом не существует, или не такой
+
+    # Сосед того, кто курит Chesterfield, держит лису.
+    # - если в доме Х-1 курят не честерфилд, и в доме Х+1 курят не честерфилд, то в доме Х не лиса
+    # - если в доме Х-1 не лиса, и в доме Х+1 не лиса, то в доме Х курят не честерфилд
+
+    # matrix - матрица связей
+    # atval1 - значение атрибута первой сущности в паре (курит:chesterfield для загадки Эйнштейна)
+    # atval2 - значение атрибута второй сущности в паре (держит:лиса для загадки Эйнштейна)
+    # distance_attr - атрибут, по которому считается расстояние (порядок_домов для загадки Эйнштейна)
+    # distance - расстояние atval1 от atval2 по distance_attr (1 для загадки Эйнштейна), строго положительное число
+    def __init__(self, matrix, atval1, atval2, distance_attr, distance):
+        assert(distance > 0)
+
+        self.matrix = matrix
+        self.atval1 = atval1
+        self.atval2 = atval2
+        self.distance_attr = distance_attr
+        self.distance = distance
+
+    def evaluate(self, relation):
+        if not relation.with_attr(self.distance_attr):
+            return None
+
+        cur_distance_val = relation.atval(self.distance_attr)
+        left_distance_val = cur_distance_val.offset_value(-self.distance)
+        right_distance_val = cur_distance_val.offset_value(+self.distance)
+        
+        if relation.with_atval(self.atval1):
+            if ((left_distance_val == None
+                    or self.matrix.is_different(Relation(left_distance_val, self.atval2)))
+                and (right_distance_val == None
+                    or self.matrix.is_different(Relation(right_distance_val, self.atval2)))):
+                return Different(relation)
+
+        if relation.with_atval(self.atval2):
+            if ((left_distance_val == None
+                    or self.matrix.is_different(Relation(left_distance_val, self.atval1)))
+                and (right_distance_val == None
+                    or self.matrix.is_different(Relation(right_distance_val, self.atval1)))):
+                return Different(relation)
+
+class Same:
+    def __init__(self, relation):
+        self.relation = relation
+
+    def evaluate(self, relation):
+        a1 = self.relation.atval1
+        a2 = self.relation.atval2
+        # same attributes
+        if not (relation.with_attr(a1.attr) and relation.with_attr(a2.attr)):
+            return None
+        # one attribute value is the same (another is not the same) - considered relation is negative
+        if relation.with_atval(a1):
+            return Different(relation)
+        if relation.with_atval(a2):
+            return Different(relation)
+        
+        return None
+
+    @classmethod
+    def of(cls, atval1, atval2):
+        return cls(Relation(atval1, atval2))
+
+class Different:
+    def __init__(self, relation):
+        self.relation = relation
+
+    def evaluate(self, relation):
+        return None
+
+    @classmethod
+    def of(cls, atval1, atval2):
+        return cls(Relation(atval1, atval2))
+    
+
+# TESTS
---
+++ einstein_tests.py
@@ -0,0 +1,301 @@
+import unittest
+
+from einstein import *
+
+class AttrTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.attr = Attr('attr', 0, ['v1', 'v2', 'v3', 'v4', 'v5'])
+
+    # value_at
+    def test_value_at_in_values_returns_value(self):
+        self.assertEqual(self.attr.value_at(2).value, 'v3')
+
+    def test_value_at_zero_returns_value(self):
+        self.assertEqual(self.attr.value_at(0).value, 'v1')
+
+    def test_value_at_last_index_returns_value(self):
+        self.assertEqual(self.attr.value_at(4).value, 'v5')
+
+    def test_value_at_negative_index_returns_none(self):
+        self.assertIsNone(self.attr.value_at(-1))
+
+    def test_value_at_index_over_max_returns_none(self):
+        self.assertIsNone(self.attr.value_at(5))
+
+class AttrValueTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.attr = Attr('attr', 0, ['v1', 'v2', 'v3', 'v4', 'v5'])
+
+    # offset_value
+    def test_offset_value_negative_offset_in_values_returns_value(self):
+        self.assertEqual(self.attr.value_at(2).offset_value(-2).value, 'v1')
+
+    def test_offset_value_positive_offset_in_values_returns_value(self):
+        self.assertEqual(self.attr.value_at(2).offset_value(2).value, 'v5')
+
+    def test_offset_value_negative_offset_out_of_values_returns_none(self):
+        self.assertIsNone(self.attr.value_at(0).offset_value(-1))
+
+    def test_offset_value_positive_offset_out_of_values_returns_none(self):
+        self.assertIsNone(self.attr.value_at(4).offset_value(1))
+
+
+class RelationTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.attr_a = Attr('attrA', 0, ['a1', 'a2', 'a3', 'a4', 'a5'])
+        cls.attr_b = Attr('attrB', 1, ['b1', 'b2', 'b3', 'b4', 'b5'])
+        cls.attr_c = Attr('attrC', 2, ['c1', 'c2', 'c3', 'c4', 'c5'])
+
+        cls.rel = Relation(cls.attr_a.a2, cls.attr_b.b3)
+
+    def test_with_attr_attr_a_in_relation_returns_true(self):
+        self.assertTrue(self.rel.with_attr(self.attr_a))
+
+    def test_with_attr_attr_b_in_relation_returns_true(self):
+        self.assertTrue(self.rel.with_attr(self.attr_b))
+
+    def test_with_attr_attr_c_not_in_relation_returns_false(self):
+        self.assertFalse(self.rel.with_attr(self.attr_c))
+
+    def test_with_atval_a2_same_attr_and_value_returns_true(self):
+        self.assertTrue(self.rel.with_atval(self.attr_a.a2))
+
+    def test_with_atval_b3_same_attr_and_value_returns_true(self):
+        self.assertTrue(self.rel.with_atval(self.attr_b.b3))
+    
+    def test_with_atval_a1_same_attr_diff_value_returns_false(self):
+        self.assertFalse(self.rel.with_atval(self.attr_a.a1))
+
+    def test_with_atval_b4_same_attr_diff_value_returns_false(self):
+        self.assertFalse(self.rel.with_atval(self.attr_b.b4))
+    
+    def test_with_atval_c1_diff_attr_returns_false(self):
+        self.assertFalse(self.rel.with_atval(self.attr_c.c1))
+    
+class ExclusiveTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.A = Attr('A', 0, ['A1', 'A2', 'A3'])
+        cls.B = Attr('B', 1, ['B1', 'B2', 'B3'])
+
+    def test_exclusive_returns_same_when_other_options_are_excluded(self):
+        m = Solver().add(Different.of(self.A.A2, self.B.B1)) \
+                    .add(Different.of(self.A.A2, self.B.B3))
+        rule_under_test = Exclusive(m)
+        result = rule_under_test.evaluate(Relation(self.A.A2, self.B.B2))
+        self.assertIsInstance(result, Same)
+        self.assertEqual(result.relation, Relation(self.A.A2, self.B.B2))
+
+    def test_exclusive_returns_none_when_not_all_other_options_are_excluded(self):
+        m = Solver().add(Different.of(self.A.A2, self.B.B1))
+        rule_under_test = Exclusive(m)
+        result = rule_under_test.evaluate(Relation(self.A.A2, self.B.B2))
+        self.assertIsNone(result)
+
+class OffsetTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.color = Attr('color', 0, ['red', 'green', 'white', 'yellow', 'blue'])
+        cls.house = Attr('house', 1, [1, 2, 3, 4, 5])
+        cls.smoke = Attr('smoke', 2, ['oldgold', 'kool', 'chesterfield', 'luckystrike', 'parliament'])
+     # дом не такой = дом не существует, или не такой
+
+    # Зелёный дом стоит сразу справа от белого дома.
+    # - если дом X-1 - не белый, то дом X - не зеленый| (X, зеленый): if ~(X-1, белый) -> Different
+    def test_left_not_white_returns_this_not_green(self):
+        matrix = Solver().add(Different.of(self.house[2], self.color.white))
+        rule = Offset(matrix, self.color.green, self.color.white, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.color.green))
+        self.assertIsInstance(result, Different)
+
+    # - если дом X+1 - не зеленый, то дом X - не белый
+    def test_right_not_green_returns_this_not_white(self):
+        matrix = Solver().add(Different.of(self.house[4], self.color.green))
+        rule = Offset(matrix, self.color.green, self.color.white, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.color.white))
+        self.assertIsInstance(result, Different)
+
+    def test_not_offset_attr_returns_none(self):
+        matrix = Solver().add(Different.of(self.house[4], self.color.green))
+        rule = Offset(matrix, self.color.green, self.color.white, self.house, 1)
+        result = rule.evaluate(Relation(self.smoke.kool, self.color.white))
+        self.assertIsNone(result)
+
+    def test_not_atval_returns_none(self):
+        matrix = Solver().add(Different.of(self.house[4], self.color.green))
+        rule = Offset(matrix, self.color.green, self.color.white, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.color.yellow))
+        self.assertIsNone(result)
+
+    def test_no_left_returns_this_not_green(self):
+        matrix = Solver()
+        rule = Offset(matrix, self.color.green, self.color.white, self.house, 1)
+        result = rule.evaluate(Relation(self.house[1], self.color.green))
+        self.assertIsInstance(result, Different)
+
+    def test_no_right_returns_this_not_white(self):
+        matrix = Solver()
+        rule = Offset(matrix, self.color.green, self.color.white, self.house, 1)
+        result = rule.evaluate(Relation(self.house[5], self.color.white))
+        self.assertIsInstance(result, Different)
+
+    def test_atval1_left_can_be_white_returns_none(self):
+        matrix = Solver()
+        rule = Offset(matrix, self.color.green, self.color.white, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.color.green))
+        self.assertIsNone(result)
+
+    def test_atval2_right_can_be_green_returns_none(self):
+        matrix = Solver()
+        rule = Offset(matrix, self.color.green, self.color.white, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.color.white))
+        self.assertIsNone(result)
+
+
+
+class DistanceTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.animal = Attr('animal', 0, ['dog', 'snail', 'fox', 'horse', 'zebra'])
+        cls.house = Attr('house', 1, [1, 2, 3, 4, 5])
+        cls.smoke = Attr('smoke', 2, ['oldgold', 'kool', 'chesterfield', 'luckystrike', 'parliament'])
+        cls.drink = Attr('drink', 3, ['coffee', 'tea', 'milk', 'orangejuice', 'water'])
+
+    # дом не такой = дом не существует, или не такой
+
+    # Сосед того, кто курит Chesterfield, держит лису.
+    # - если в доме Х-1 курят не честерфилд, и в доме Х+1 курят не честерфилд, то в доме Х не лиса
+    # - если в доме Х-1 не лиса, и в доме Х+1 не лиса, то в доме Х курят не честерфилд
+
+    def test_left_right_not_chesterfield_returns_this_not_fox(self):
+        matrix = Solver().add(Different.of(self.house[2], self.smoke.chesterfield)) \
+                         .add(Different.of(self.house[4], self.smoke.chesterfield))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.animal.fox))
+        self.assertIsInstance(result, Different)
+
+    def test_left_not_chesterfield_right_not_exist_returns_this_not_fox(self):
+        matrix = Solver().add(Different.of(self.house[4], self.smoke.chesterfield))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[5], self.animal.fox))
+        self.assertIsInstance(result, Different)
+
+    def test_left_not_exist_right_not_chesterfield_returns_this_not_fox(self):
+        matrix = Solver().add(Different.of(self.house[2], self.smoke.chesterfield))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[1], self.animal.fox))
+        self.assertIsInstance(result, Different)
+
+    def test_left_right_not_fox_returns_this_not_chesterfield(self):
+        matrix = Solver().add(Different.of(self.house[2], self.animal.fox)) \
+                         .add(Different.of(self.house[4], self.animal.fox))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.smoke.chesterfield))
+        self.assertIsInstance(result, Different)
+
+    def test_left_not_fox_right_not_exist_returns_this_not_chesterfield(self):
+        matrix = Solver().add(Different.of(self.house[4], self.animal.fox))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[5], self.smoke.chesterfield))
+        self.assertIsInstance(result, Different)
+
+    def test_left_not_exist_right_not_fox_returns_this_not_chesterfield(self):
+        matrix = Solver().add(Different.of(self.house[2], self.animal.fox))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[1], self.smoke.chesterfield))
+        self.assertIsInstance(result, Different)
+
+    def test_not_with_distance_attr_returns_none(self):
+        matrix = Solver().add(Different.of(self.house[2], self.smoke.chesterfield)) \
+                         .add(Different.of(self.house[4], self.smoke.chesterfield))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.drink.tea, self.animal.fox))
+        self.assertIsNone(result)
+
+    def test_not_with_chesterfield_returns_none(self):
+        matrix = Solver().add(Different.of(self.house[2], self.animal.fox)) \
+                         .add(Different.of(self.house[4], self.animal.fox))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.smoke.kool))
+        self.assertIsNone(result)
+
+    def test_chesterfield_left_not_different_returns_none(self):
+        matrix = Solver().add(Different.of(self.house[4], self.animal.fox))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.smoke.chesterfield))
+        self.assertIsNone(result)
+
+    def test_chesterfield_right_not_different_returns_none(self):
+        matrix = Solver().add(Different.of(self.house[2], self.animal.fox))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.smoke.chesterfield))
+        self.assertIsNone(result)
+
+    def test_not_with_fox_returns_none(self):
+        matrix = Solver().add(Different.of(self.house[2], self.smoke.chesterfield)) \
+                         .add(Different.of(self.house[4], self.smoke.chesterfield))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.animal.zebra))
+        self.assertIsNone(result)
+
+    def test_fox_left_not_different_returns_none(self):
+        matrix = Solver().add(Different.of(self.house[4], self.smoke.chesterfield))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.animal.fox))
+        self.assertIsNone(result)
+
+    def test_fox_right_not_different_returns_none(self):
+        matrix = Solver().add(Different.of(self.house[2], self.smoke.chesterfield))
+        rule = Distance(matrix, self.smoke.chesterfield, self.animal.fox, self.house, 1)
+        result = rule.evaluate(Relation(self.house[3], self.animal.fox))
+        self.assertIsNone(result)
+
+class SameTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.A = Attr('A', 0, [1, 2, 3])
+        cls.B = Attr('B', 1, [1, 2, 3])
+
+    def test_returns_different_when_same_row(self):
+        rule_under_test = Same.of(self.A[2], self.B[2])
+        result = rule_under_test.evaluate(Relation(self.A[2], self.B[1]))
+        self.assertIsInstance(result, Different)
+   
+    def test_returns_different_when_same_column(self):
+        rule_under_test = Same.of(self.A[2], self.B[2])
+        result = rule_under_test.evaluate(Relation(self.A[3], self.B[2]))
+        self.assertIsInstance(result, Different)
+   
+    def test_returns_none_when_not_same_row_or_column(self):
+        rule_under_test = Same.of(self.A[2], self.B[2])
+        result = rule_under_test.evaluate(Relation(self.A[1], self.B[3]))
+        self.assertIsNone(result)
+
+class SolverAcceptanceTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.house = Attr('house', 0, [1, 2, 3, 4, 5])
+        cls.color = Attr('color', 1, ['red', 'green', 'white', 'yellow', 'blue'])
+        cls.nation = Attr('nation', 2, ['english', 'spanish', 'ukrainian', 'norwegian', 'japanese'])
+        cls.animal = Attr('animal', 3, ['dog', 'snail', 'fox', 'horse', 'zebra'])
+        cls.drink = Attr('drink', 4, ['coffee', 'tea', 'milk', 'orangejuice', 'water'])
+        cls.smoke = Attr('smoke', 5, ['oldgold', 'kool', 'chesterfield', 'luckystrike', 'parliament'])
+
+        cls.solver = Solver()
+        cls.solver.add(Same.of(cls.color.red, cls.nation.english))
+        cls.solver.add(Same.of(cls.nation.spanish, cls.animal.dog))
+        cls.solver.add(Same.of(cls.color.green, cls.drink.coffee))
+        cls.solver.add(Same.of(cls.nation.ukrainian, cls.drink.tea))
+
+        cls.solver.add(Same.of(cls.smoke.oldgold, cls.animal.snail))
+
+    def test_added_relation_exists(self):
+        self.assertIsInstance(self.solver.get(Relation(self.nation.english, self.color.red)), Same)
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
