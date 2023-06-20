c1f69b9e3245b1a1f35f5613e93a26f09c4762ed
kaerber <kaerber@gmail.com>
2023-06-20 20:16:09 +0200

    solved the easier version

--- einstein_run.py
+++ einstein_run.py
@@ -8,7 +8,7 @@ drink = Attr('drink', 4, ['coffee', 'tea', 'milk', 'orangejuice', 'water'])
 smoke = Attr('smoke', 5, ['oldgold', 'kool', 'chesterfield', 'luckystrike', 'parliament'])
 
 solver = Solver([house, color, nation, animal, drink, smoke])
-solver.debug = True
+#solver.debug = True
 
 solver.add(Exclusive())
 
@@ -16,7 +16,7 @@ solver.add(Same.of(color.red, nation.english))
 solver.add(Same.of(nation.spanish, animal.dog))
 solver.add(Same.of(color.green, drink.coffee))
 solver.add(Same.of(nation.ukrainian, drink.tea))
-solver.add(Offset(color.green, color.white, house, 1))
+solver.add(Offset(color.green, color.white, house, -1))
 solver.add(Same.of(smoke.oldgold, animal.snail))
 solver.add(Same.of(color.yellow, smoke.kool))
 solver.add(Same.of(house[3], drink.milk))
@@ -27,11 +27,19 @@ solver.add(Same.of(smoke.luckystrike, drink.orangejuice))
 solver.add(Same.of(nation.japanese, smoke.parliament))
 solver.add(Distance(nation.norwegian, color.blue, house, 1))
 
-turn_count = 4
+
 i = 1
-while solver.iter() and i <= turn_count:
-    print()
-    print("turn", i)
-    print()
+while solver.iter():
+    if solver.debug:
+        print()
+        print("turn", i)
+        print()
     i += 1
-print("turn_count", turn_count)
+
+for man in nation.ordered_values:
+    # who drinks water
+    if solver.is_same(Relation(drink.water, man)):
+        print(man.value, "drinks water.")
+    # who keeps zebra
+    if solver.is_same(Relation(animal.zebra, man)):
+        print(man.value, "keeps zebra.")
